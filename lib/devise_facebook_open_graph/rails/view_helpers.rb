353d31da48de6e46e7d5ff30ec263afc60a25ab6
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-06-13 13:36:58 +0200

    Updated the documentation again with the next how-to-get-started-step.

--- lib/devise_facebook_open_graph/rails/view_helpers.rb
+++ lib/devise_facebook_open_graph/rails/view_helpers.rb
@@ -3,7 +3,12 @@
 module DeviseFacebookOpenGraph
   module Rails
     module ViewHelpers
-
+      #
+      # Inserts facebook HTML and javascript tag for initializing
+      # JavaScript SDK. See http://developers.facebook.com/docs/authentication/: Single Sign-on.
+      #
+      # Some options to this helper-method might be added in the future :-)
+      #
       def facebook_init_javascript_sdk
         buffer = content_tag :div, '', :id => 'fb-root'
         buffer << javascript_include_tag(DeviseFacebookOpenGraph::Facebook::Config.sdk_java_script_source)
