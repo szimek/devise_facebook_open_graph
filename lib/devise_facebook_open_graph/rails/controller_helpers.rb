38871e6980b26f5caa1582434095547b01d5e53b
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-06-15 11:35:29 +0200

    Exposed Facebook session in the Application controller.
    It is also made a helper method accessible through the views.

--- lib/devise_facebook_open_graph/rails/controller_helpers.rb
+++ lib/devise_facebook_open_graph/rails/controller_helpers.rb
@@ -3,6 +3,15 @@
 module DeviseFacebookOpenGraph
   module Rails
     module ControllerHelpers
+      extend ActiveSupport::Concern
+
+      included do
+        helper_method :facebook_session
+      end
+
+      def facebook_session
+        @facebook_session ||= DeviseFacebookOpenGraph::Facebook::Session.new(cookies)
+      end
     end
   end
 end
